9c3dbd23c6eabac4e7327cc5d23a9ade546780fe
Sylvain Rivierre <sylvainrivierre@gmail.com>
2015-01-14 14:33:12 +0100

    Refactor rights and config page

--- Chrome/app/scripts/run.js
+++ Chrome/app/scripts/run.js
@@ -8,127 +8,142 @@ var socketInfo;
 
 var extensionId;
 
+var port = 8000;
+
 function runServer() {
 
-	chrome.runtime.onMessageExternal.addListener(
-		function (request, sender, sendResponse) {
-		if (request) {
-			if (request.id) {
-				console.log("extension registered");
-				extensionId = request.id;
-			}
-		}
-	});
+    chrome.runtime.onMessageExternal.addListener(
+            function (request, sender, sendResponse) {
+                if (request) {
+                    if (request.id) {
+                        console.log("extension registered");
+                        extensionId = request.id;
+                    }
+                }
+            });
 
-	socket.create('tcp',
-		function (socketInfoI) {
-		socketHttpId = socketInfoI.socketId;
-		socketInfo = socketInfoI;
-		socket.listen(socketHttpId, '127.0.0.1', 8000, function (result) {
-			socket.accept(socketHttpId, onAccept);
-		});
-	});
-	
-	keepAlive();
+    socket.create('tcp',
+            function (socketInfoI) {
+                socketHttpId = socketInfoI.socketId;
+                socketInfo = socketInfoI;
+                socket.listen(socketHttpId, '127.0.0.1', port, function (result) {
+                    socket.accept(socketHttpId, onAccept);
+                });
+            });
+
+    keepAlive();
 }
 
-function keepAlive(){
+function keepAlive() {
 
-	setTimeout(keepAlive,1000);
+    setTimeout(keepAlive, 1000);
 }
 
 function onAccept(acceptInfo) {
-	readFromSocket(acceptInfo.socketId);
+    readFromSocket(acceptInfo.socketId);
 }
 
 var arrayBufferToString = function (buffer) {
-	var str = '';
-	var uArrayVal = new Uint8Array(buffer);
-	for (var s = 0; s < uArrayVal.length; s++) {
-		str += String.fromCharCode(uArrayVal[s]);
-	}
-	return str;
+    var str = '';
+    var uArrayVal = new Uint8Array(buffer);
+    for (var s = 0; s < uArrayVal.length; s++) {
+        str += String.fromCharCode(uArrayVal[s]);
+    }
+    return str;
 };
 
 var stringToUint8Array = function (string) {
-	var buffer = new ArrayBuffer(string.length);
-	var view = new Uint8Array(buffer);
-	for (var i = 0; i < string.length; i++) {
-		view[i] = string.charCodeAt(i);
-	}
-	return view;
+    var buffer = new ArrayBuffer(string.length);
+    var view = new Uint8Array(buffer);
+    for (var i = 0; i < string.length; i++) {
+        view[i] = string.charCodeAt(i);
+    }
+    return view;
 };
 
 var readFromSocket = function (socketId) {
-	socket.read(socketId, function (readInfo) {
-		var data = arrayBufferToString(readInfo.data);
-		if (data.indexOf("GET ") == 0) {
-			var keepAlive = false;
-			if (data.indexOf("Connection: keep-alive") != -1) {
-				keepAlive = true;
-			}
-			var uriEnd = data.indexOf(" ", 4);
-			if (uriEnd < 0) {
-				return;
-			}
-			var uri = data.substring(4, uriEnd);
+    socket.read(socketId, function (readInfo) {
+        var data = arrayBufferToString(readInfo.data);
+        if (data.indexOf("GET ") === 0) {
+            var keepAlive = false;
+            if (data.indexOf("Connection: keep-alive") !== -1) {
+                keepAlive = true;
+            }
+            var uriEnd = data.indexOf(" ", 4);
+            if (uriEnd < 0) {
+                return;
+            }
+            var uri = data.substring(4, uriEnd);
 
-			var cmdPos = uri.indexOf("?");
-			if (cmdPos == -1 || uri.indexOf("music") != 1) {
-				writeErrorResponse(socketId, 503, keepAlive, uri);
-				console.log("command error : " + uri);
-			} else {
-				write200Response(socketId, keepAlive);
-				uri = uri.substring(1);
-				console.log("command : " + uri);
+            var cmdPos = uri.indexOf("?");
+            if (cmdPos === -1 || uri.indexOf("music") !== 1) {
+                writeErrorResponse(socketId, 503, keepAlive, uri);
+                console.log("command error : " + uri);
+            } else {
+                write200Response(socketId, keepAlive);
+                uri = uri.substring(1);
+                console.log("command : " + uri);
 
-				var action = {
-					action : uri.substring(cmdPos)
-				};
-				chrome.runtime.sendMessage(extensionId, action);
-			}
-		} else {
-			socket.destroy(socketId);
-		}
-	});
+                var action = {
+                    action: uri.substring(cmdPos)
+                };
+                if (extensionId) {
+                    chrome.runtime.sendMessage(extensionId, action);
+                } else {
+                    console.error("An extension must be registered before sending commands");
+                }
+            }
+        } else {
+            socket.destroy(socketId);
+        }
+    });
 };
 
 var writeErrorResponse = function (socketId, errorCode, keepAlive, message) {
-	var file = {
-		size : 0
-	};
-	var contentType = "text/plain";
-	var contentLength = file.size;
-	var header = stringToUint8Array("HTTP/1.0 " + errorCode);
-	var outputBuffer = new ArrayBuffer(header.byteLength + file.size);
-	var view = new Uint8Array(outputBuffer)
-		view.set(header, 0);
-	socket.write(socketId, outputBuffer, function (writeInfo) {
-		if (keepAlive) {
-			readFromSocket(socketId);
-		} else {
-			socket.destroy(socketId);
-			socket.accept(socketInfo.socketId, onAccept);
-		}
-	});
+    var file = {
+        size: 0
+    };
+    var contentType = "text/plain";
+    var contentLength = file.size;
+    var header = stringToUint8Array("HTTP/1.0 " + errorCode);
+    var outputBuffer = new ArrayBuffer(header.byteLength + file.size);
+    var view = new Uint8Array(outputBuffer);
+    view.set(header, 0);
+    socket.write(socketId, outputBuffer, function (writeInfo) {
+        if (keepAlive) {
+            readFromSocket(socketId);
+        } else {
+            socket.destroy(socketId);
+            socket.accept(socketInfo.socketId, onAccept);
+        }
+    });
 };
 
 var write200Response = function (socketId, keepAlive) {
-	var contentType = "text/plain";
-	textsize = 0;
-	var contentLength = textsize;
-	var header = stringToUint8Array("HTTP/1.0 200 OK");
-	var outputBuffer = new ArrayBuffer(header.byteLength + textsize);
-	var view = new Uint8Array(outputBuffer)
-		view.set(header, 0);
-	socket.write(socketId, outputBuffer, function (writeInfo) {
-		if (keepAlive) {
-			readFromSocket(socketId);
-		} else {
-			socket.destroy(socketId);
-			socket.accept(socketInfo.socketId, onAccept);
-		}
-	});
+    var contentType = "text/plain";
+    textsize = 0;
+    var contentLength = textsize;
+    var header = stringToUint8Array("HTTP/1.0 200 OK");
+    var outputBuffer = new ArrayBuffer(header.byteLength + textsize);
+    var view = new Uint8Array(outputBuffer);
+    view.set(header, 0);
+    socket.write(socketId, outputBuffer, function (writeInfo) {
+        if (keepAlive) {
+            readFromSocket(socketId);
+        } else {
+            socket.destroy(socketId);
+            socket.accept(socketInfo.socketId, onAccept);
+        }
+    });
 };
 
 runServer();
+
+chrome.app.runtime.onLaunched.addListener(function () {
+    chrome.app.window.create('options.html', {
+        'bounds': {
+            'width': 400,
+            'height': 500
+        }
+    });
+});
--- Chrome/extension/scripts/run.js
+++ Chrome/extension/scripts/run.js
@@ -1,71 +1,72 @@
 function init() {
 
-	chrome.runtime.onMessageExternal.addListener(
-		function (request, sender, sendResponse) {
-		if (request) {
-			if (request.action) {
-				console.log(request.action);
-				command(request.action);
-			}
-		}
-	});
+    chrome.runtime.onMessageExternal.addListener(
+            function (request, sender, sendResponse) {
+                if (request) {
+                    if (request.action) {
+                        console.log(request.action);
+                        command(request.action);
+                    }
+                }
+            });
 
-	sendExtensionId();
+    sendExtensionId();
 }
 
 function sendExtensionId() {
-	chrome.management.getAll(function (extensions) {
-		extensions.forEach(function (extension) {
+    chrome.management.getAll(function (extensions) {
+        extensions.forEach(function (extension) {
 
-			if (extension.name == "Music Control Linker" &&
-				extension.description == "Allows you to link your online music players with Music Control Extension") {
-				chrome.runtime.sendMessage(extension.id, {
-					id : chrome.runtime.id
-				});
-				console.log("send id extension to app");
-			}
-		});
-	});
+            if (extension.name === "Music Control Linker" &&
+                    extension.description === "Allows you to link your online music players with Music Control Extension") {
+                chrome.runtime.sendMessage(extension.id, {
+                    id: chrome.runtime.id
+                });
+                console.log("send id extension to app");
+            }
+        });
+    });
 
-	setTimeout(sendExtensionId, 5000);
+    setTimeout(sendExtensionId, 5000);
 }
 
 function command(action) {
-	chrome.tabs.query({
-		title : "*Google*Play*"
-	}, function (tabs) {
-		if (tabs && tabs.length > 0) {
-			control("google", tabs[0].id, action);
-		}
-	});
-	chrome.tabs.query({
-		title : "Amazon Music Library"
-	}, function (tabs) {
-		if (tabs && tabs.length > 0) {
-			control("amazon", tabs[0].id, action);
-		}
-	});
-	chrome.tabs.query({
-		title : "Pandora Internet Radio*"
-	}, function (tabs) {
-		if (tabs && tabs.length > 0) {
-			control("pandora", tabs[0].id, action);
-		}
-	});
+    chrome.tabs.query({
+        title: "*Google*Play*"
+    }, function (tabs) {
+        if (tabs && tabs.length > 0) {
+            control("google", tabs[0].id, action);
+        }
+    });
+    chrome.tabs.query({
+        title: "Amazon Music Library"
+    }, function (tabs) {
+        if (tabs && tabs.length > 0) {
+            control("amazon", tabs[0].id, action);
+        }
+    });
+    chrome.tabs.query({
+        title: "Pandora Internet Radio*"
+    }, function (tabs) {
+        if (tabs && tabs.length > 0) {
+            control("pandora", tabs[0].id, action);
+        }
+    });
 }
 
 function control(player, tabId, action) {
-	var file;
-	if (action === "Play") {
-		file = "playPause.js";
-	} else if (action === "Next") {
-		file = "next.js";
-	} else if (action === "Previous") {
-		file = "previous.js";
-	}
-	file = "scripts/" + player + "_" + file;
-	chrome.tabs.executeScript(tabId, {
-		file : file
-	});
+    var file;
+    var laction = action.toLowerCase();
+    if (laction === "play") {
+        file = "playPause.js";
+    } else if (laction === "next") {
+        file = "next.js";
+    } else if (laction === "previous") {
+        file = "previous.js";
+    }
+    file = "scripts/" + player + "_" + file;
+    chrome.tabs.executeScript(tabId, {
+        file: file
+    });
 }
 init();
